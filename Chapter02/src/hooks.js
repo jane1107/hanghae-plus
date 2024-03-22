6e7a1dc7723c4d6a32dec1b52264b907ee3e0fd9
hyeonjeong <hjung9248@gmail.com>
2024-03-22 23:21:19 +0900

    implement useState and useMemo

--- Chapter02/src/hooks.js
+++ Chapter02/src/hooks.js
@@ -1,15 +1,45 @@
 export function createHooks(callback) {
+  const _callback = callback;
+  let _currentIndex = 0;
+  let _states = [];
+
   const useState = (initState) => {
-    return [];
+    const _key = _currentIndex;
+    if (_states.length === _currentIndex) {
+      _states[_currentIndex] = initState;
+    }
+    const state = _states[_key];
+    const setState = (newVal) => {
+      if (_states[_key] === newVal) return;
+      _states[_key] = newVal;
+      _callback();
+    };
+    _currentIndex += 1;
+    return [state, setState];
   };
 
+  const memorizedStates = [];
   const useMemo = (fn, refs) => {
-    return fn();
+    if (!memorizedStates[_currentIndex]) {
+      const newVal = fn();
+      memorizedStates[_currentIndex] = [newVal, refs];
+      _currentIndex += 1;
+      return newVal;
+    }
+    const nextRefs = refs;
+    const [curVal, curRefs] = memorizedStates[_currentIndex];
+    if (curRefs.every((ref, index) => ref === nextRefs[index])) {
+      return curVal;
+    }
+    const newVal = fn();
+    memorizedStates[_currentIndex] = [newVal, refs];
+    _currentIndex += 1;
+    return newVal;
   };
 
   const resetContext = () => {
-
-  }
+    _currentIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
