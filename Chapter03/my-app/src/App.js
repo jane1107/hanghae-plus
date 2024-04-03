5fb1caa72bebef7a9efa38a67661b69b5fa9240b
hyeonjeong <hjung9248@gmail.com>
2024-04-04 00:21:00 +0900

    add more contents

--- Chapter03/my-app/src/App.js
+++ Chapter03/my-app/src/App.js
@@ -19,20 +19,26 @@ function App() {
         <meta name="twitter:image" content={url} />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <hr />
+      <h3>SEO란 Search Engine Optimization의 줄임말</h3>
+      <li>검색 엔진이 콘텐츠를 이해하도록 돕고</li>
+      <li>사용자가 사이트를 찾도록 돕고</li>
+      <li>검색엔진을 통해 사이트를 방문할지 여부를 결정하도록 도움</li>
+      <a href="https://developers.google.com/search/docs/fundamentals/seo-starter-guide?hl=ko">
+        구글 참고
+      </a>
+
+      <h4>
+        메타 태그나 favicon 은 질 좋은 컨텐츠를 제공하는 것 중에 일부분일 뿐.
+        <br />
+        좋은 컨텐츠를 담고 있는 것이 중요하다는 것 알았다.
+        <br />
+        컨텐츠가 너무 없어도 useful information 을 제공하지 않는다며 SEO가
+        낮아진다.
+        <br />
+        이정도면 250글자가 됐으려나?!
+      </h4>
     </div>
   );
 }
