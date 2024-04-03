bc28883e9fb0a8a16d913870d0ca5ae1bf4e9fff
hyeonjeong <hjung9248@gmail.com>
2024-04-03 22:33:34 +0900

    add twitter tag

--- Chapter03/my-app/src/App.js
+++ Chapter03/my-app/src/App.js
@@ -3,20 +3,21 @@ import "./App.css";
 import { Helmet } from "react-helmet";
 
 function App() {
+  let title = "enhance seo";
+  let description = "enhance seo in your react application";
+  let url = "https://jane1107.github.io/";
   return (
     <div className="App">
       <Helmet>
-        <meta
-          name="problem A"
-          content="enhance seo in your react application"
-        />
+        <meta name="problem A" content={description} />
         <meta property="og:site_name" content="Problem_A" />
-        <meta property="og:title" content="enhance seo" />
-        <meta property="og:url" content="https://jane1107.github.io/" />
-        <meta
-          property="og:description"
-          content="enhance seo in your react application"
-        />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={url} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={url} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
